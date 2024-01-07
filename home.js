5c23ec263e91c9c7046c37b5fe5393f30edba03a
DorantVelija <dorantvelija@outlook.com>
2024-01-07 03:18:23 +0100

    Initial commit

---
+++ home.js
@@ -0,0 +1,9 @@
+
+
+
+
+document.getElementById("about").addEventListener("click", function() {
+    var aboutSection = document.querySelector(".section2");
+    aboutSection.scrollIntoView({ behavior: "smooth"});
+});
+
