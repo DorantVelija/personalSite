6261b4698c942306244782681647daf640ef04cb
DorantVelija <dorantvelija@outlook.com>
2024-01-08 02:10:34 +0100

    Your commit message here

--- home.js
+++
@@ -1,9 +0,0 @@
-
-
-
-
-document.getElementById("about").addEventListener("click", function() {
-    var aboutSection = document.querySelector(".section2");
-    aboutSection.scrollIntoView({ behavior: "smooth"});
-});
-
