bbf6d77677c7e9b19bb192e7cacd0aa4b3aef219
DorantVelija <dorantvelija@outlook.com>
2024-01-15 19:07:03 +0100

    a

---
+++ home.js
@@ -0,0 +1,19 @@
+const questions = [
+    { name: 'scratch',
+      question: [                                                       // 0 
+        'saf','safsaf','safsafsafas','safkugwqfakfsajfbsaf'
+      ]}
+      
+
+
+      ,
+      {
+        name: 'sql',
+        question: [
+            'safsaf','saf','saf safsafas','safkugwqfak'                 // 1
+        ]
+      }
+];
+
+console.log(questions[0].question[0])
+
