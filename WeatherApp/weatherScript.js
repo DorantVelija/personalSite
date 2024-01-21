05e92ab57b7ea858c080c5509be4768eca2524fa
DorantVelija <dorantvelija@outlook.com>
2024-01-21 20:01:45 +0100

    added different icons

--- WeatherApp/weatherScript.js
+++ WeatherApp/weatherScript.js
@@ -6,16 +6,41 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 
 const citySearch = document.querySelector('#citySearch');
 const searchBtn = document.querySelector('#searchBtn');
+const weatherIcon = document.querySelector('.weatherIconC');
 
 async function getWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
-  //  console.log(data);
 
     document.querySelector('.cityName').innerHTML = data.name;
     document.querySelector('#temp').innerHTML = Math.round(data.main.temp);
-    document.querySelector('#feelLike').innerHTML = Math.round(data.main.feels_like);
+    document.querySelector('#feelLike').innerHTML = Math.round(data.main.feels_like)
+    document.querySelector('.condition').innerHTML = data.weather[0].main;
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "/WeatherApp/icons/weathericons/cloud.png";
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "/WeatherApp/icons/weathericons/clear.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "/WeatherApp/icons/weathericons/raincloud.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "/WeatherApp/icons/weathericons/raincloud.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "/WeatherApp/icons/weathericons/cloud.png";
+    }
+    else if(data.weather[0].main == "Snow"){
+        weatherIcon.src = "WeatherApp/icons/weathericons/snow.png";
+    }
+    else if(data.weather[0].main == "Thunderstorm"){
+        weatherIcon.src = "WeatherApp/icons/weathericons/lightning.png";
+    }
+
+    
 }
 searchBtn.addEventListener('click', () => {
     getWeather(citySearch.value);
